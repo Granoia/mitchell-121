f39c72e9127e979047eeed24471f33b50b34c1b4
Granoia <nicholasfranzese2026@u.northwestern.edu>
2023-10-20 20:28:43 -0700

    first lesson: print, arithmetic, math library, integers and floats, binary, functions

--- hello.go
+++ hello.go
@@ -2,7 +2,30 @@ package main
 
 import "fmt"
 
-func main() {
-	fmt.Println("Hello World!")
+func cube(x int) int {
+	return x * x * x
+}
 
+func eatass(fodmap float64) float64 {
+	fodmap = fodmap * 4
+	fmt.Println(fodmap)
+	fodmap = fodmap + 1
+	return fodmap + 5
+}
+
+func fUCK(x int, y int, z int) int {
+	return x + y + z
+}
+
+func dickbutt(dick int, butt int) int {
+	return dick + butt
+}
+
+func main() {
+	var dank int = 33
+	var real int = 36
+	var meme int
+	meme = dickbutt(dank, real)
+	fmt.Println(meme)
+	fmt.Println("nice")
 }
