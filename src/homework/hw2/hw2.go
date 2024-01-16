0600f884637743ac32f8070bf280713994b8fbdb
Josh Hepworth <zeplar1@gmail.com>
2024-01-15 18:39:11 -0600

    hw2 function defs

--- src/homework/hw2/hw2.go
+++ src/homework/hw2/hw2.go
@@ -1,66 +1,22 @@
 package hw2
 
-import "math"
-
 // IsPrime returns true if the argument is prime.
-func IsPrime(x int) bool {
-	if x < 2 {
-		return false
-	}
-	for i := 2; i < x; i += 2 {
-		if x%i == 0 {
-			return false
-		}
-	}
-	return true
+func IsPrime() {
+
 }
 
-func SumCubes(n int) int {
-	var sum int
-	for i := 0; i <= n; i++ {
-		sum += i * i * i
-	}
-	return sum
+// SumCubes returns the sum of the cubes of the first n numbers.
+func SumCubes() {
 }
 
-func Fibonacci3(n int) int {
-	x := 1
-	y := 1
-	z := 1
-	for i := 3; i < n; i++ {
-		nextNumber := x + y + z
-		x = y
-		y = z
-		z = nextNumber
-	}
-	return z
+// Fibonacci returns the nth Fibonacci3 number.
+func Fibonacci3() {
 }
 
-func poly(x, y float64) float64 {
-	return -math.Pow(x, 4) + 3*math.Pow(x, 2) - math.Pow(y, 4) + 5*math.Pow(y, 2)
-}
-func Polymax(xmin, xmax, ymin, ymax int) int {
-	max := math.MinInt
-	for x := xmin; x <= xmax; x++ {
-		for y := ymin; y <= ymax; y++ {
-			n := int(poly(float64(x), float64(y)))
-			if n > max {
-				max = n
-			}
-		}
-	}
-	return max
+// Polymax returns the maximum of a polynomial function -x^4 + 3x^2 -y^4 + 5y^2.
+func Polymax() {
 }
 
-func Collatz(n int) int {
-	c := 1
-	for n != 1 {
-		c++
-		if n%2 == 1 {
-			n = 3*n + 1
-		} else {
-			n = n / 2
-		}
-	}
-	return c
+// Collatz returns the number of steps it takes to reach 1 under Collatz's conjecture.
+func Collatz() {
 }
